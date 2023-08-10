aad229bdb81822385885c79a516e2429924b018a
Jose <jmmartinezastroza@gmail.com>
2023-08-10 19:28:11 -0400

    RA00: Add configuration files

---
+++ app/src/main/java/com/example/pokeapi/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pokeapi.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dogapi.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
