5c2bba7c7be1d67ca0900ea676a097e46594d3d6
Jose <jmmartinezastroza@gmail.com>
2023-08-21 18:21:24 -0400

    Fix03: add binding fixing to pokemon detail view

--- app/src/main/java/com/example/pokeapi/ui/PokemonDetailFragment.kt
+++ app/src/main/java/com/example/pokeapi/ui/PokemonDetailFragment.kt
@@ -1,11 +1,10 @@
 package com.example.pokeapi.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
 import androidx.navigation.fragment.navArgs
 import com.example.pokeapi.R
 import com.example.pokeapi.core.Resource
@@ -14,7 +13,6 @@ import com.example.pokeapi.databinding.FragmentPokemonDetailBinding
 import com.example.pokeapi.domain.PokemonRepositoryImp
 import com.example.pokeapi.domain.RetrofitClient
 import com.example.pokeapi.presentation.PokemonDetailsViewModel
-import com.example.pokeapi.presentation.PokemonViewModel
 import com.squareup.picasso.Picasso
 
 class PokemonDetailFragment : Fragment(R.layout.fragment_pokemon_detail) {
@@ -31,49 +29,34 @@ class PokemonDetailFragment : Fragment(R.layout.fragment_pokemon_detail) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding= FragmentPokemonDetailBinding.bind(view)
-        binding.textView2.setText(args.pokemonName)
-        var hp: Int=0
-        var defense: Int=0
-        var attack: Int=0
-        var types: String=""
-        var weight: Int=0
-        var moves: String=""
+        binding = FragmentPokemonDetailBinding.bind(view)
+        binding.textPokemonName.text = args.pokemonName.uppercase()
 
-        var typses: String=""
         viewModel.fetchPokemonDetails(args.pokemonName).observe(viewLifecycleOwner, Observer {
-            when(it) {
+            when (it) {
                 is Resource.Loading -> {
-                    Picasso.get().load(R.drawable.baseline_access_time_filled_24)
+                    Picasso.get().load(R.drawable.baseline_access_time_filled_24).into(binding.imgPokemon)
                 }
                 is Resource.Success -> {
-                    Picasso.get().load(it.data.sprites.front_default).into(binding.imgPokemon)
+                    val pokemon = it.data
 
-                    for (elem in it.data.types) {
-                        types= elem.type.name+" "+types
-                    }
-                    binding.txtElementValue.setText(types)
-                    weight=it.data.weight
-                    binding.txtWeightValue.setText(weight)
-                    hp=it.data.stats.get(0).baseStat
-                    binding.txtHpValue.setText(hp)
-                    it.data.stats.get(0).stat.name
-                    attack=it.data.stats.get(1).baseStat
-                    binding.txtAttackValue.setText(attack)
-                    defense=it.data.stats.get(2).baseStat
-                    binding.txtDefenseValue.setText(defense)
+                    Picasso.get().load(pokemon.sprites.front_default).into(binding.imgPokemon)
 
-                    for (elem in it.data.moves) {
-                        moves= elem.move.name+" "+moves
-                    }
-                    binding.txtMovesNames.setText(moves)
+                    val types = pokemon.types.joinToString(", ") { it.type.name }
+                    binding.txtElementValue.text = types
+                    binding.txtHeightValue.text = pokemon.height.toString()
+                    binding.txtWeightValue.text = pokemon.weight.toString()
+                    binding.txtHpValue.text = pokemon.stats[0].baseStat.toString()
+                    binding.txtAttackValue.text = pokemon.stats[1].baseStat.toString()
+                    binding.txtDefenseValue.text = pokemon.stats[2].baseStat.toString()
+
+                    val moves = pokemon.moves.joinToString(" ,") { it.move.name }
+                    binding.txtMovesNames.text = moves
                 }
                 is Resource.Failure -> {
-/*                    binding.pokemonLoadingRecycler.visibility=View.GONE*/
+                    /* Handle failure state if needed */
                 }
-
             }
-
         })
     }
 }
